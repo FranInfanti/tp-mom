9c95878061528f057b3a10f5b76871a57f7b377d
FranInfanti <francisco.infanti@gmail.com>
2026-09-05 18:41:07 +0000

    feat: implements abstract methods for MessageMiddlewareQueueRabbitMQ

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,12 +1,74 @@
 import pika
 import random
 import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+
+from .middleware import (
+    MessageMiddlewareQueue,
+    MessageMiddlewareExchange,
+    MessageMiddlewareMessageError,
+    MessageMiddlewareDisconnectedError,
+    MessageMiddlewareCloseError,
+    MessageMiddlewareDeleteError,
+)
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.queue_name = queue_name
+
+        # creates a conn with RabbitMQ broker
+        self.conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+
+        # defines a chan that allows to communicate with RabbitMQ broker
+        self.chan = self.conn.channel()
+
+        # declare the queue cause it might not exist yet
+        self.chan.queue_declare(queue_name)
+
+    def start_consuming(self, on_message_callback):
+        def callback(ch, method, properties, body):
+            on_message_callback(
+                message=body, 
+                ack=lambda: ch.basic_ack(delivery_tag=method.delivery_tag),
+                nack=lambda: ch.basic_nack(delivery_tag=method.delivery_tag)
+            )
+
+        try:
+            self.chan.basic_consume(
+                queue=self.queue_name,
+                on_message_callback=callback,
+                auto_ack=False
+            )
+
+            self.chan.start_consuming()
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception:
+            raise MessageMiddlewareMessageError()
+
+    def stop_consuming(self):
+        try:
+            self.chan.stop_consuming()
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+
+    def send(self, message):
+        try:
+            self.chan.basic_publish(
+                exchange='',
+                routing_key=self.queue_name,
+                body=message
+            )
+        except pika.exceptions.AMQPConnectionError:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception:
+            raise MessageMiddlewareMessageError()
+
+    def close(self):
+        try:
+            self.conn.close()
+        except Exception:
+            raise MessageMiddlewareCloseError()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
